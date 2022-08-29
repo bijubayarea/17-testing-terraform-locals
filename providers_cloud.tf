0b6e9d823c1b1ca98533f7ec964007be099f9195
bijubayarea <103339956+bijubayarea@users.noreply.github.com>
2022-08-28 22:25:54 -0700

    Update providers_cloud.tf
    removed aws profile

--- providers_cloud.tf
+++ providers_cloud.tf
@@ -2,5 +2,5 @@ provider "aws" {
   #shared_config_files      = ["/Users/tf_user/.aws/conf"]
   region                   = var.aws_region
   #shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "vscode-user"
+  #profile                  = "vscode-user"
 }
